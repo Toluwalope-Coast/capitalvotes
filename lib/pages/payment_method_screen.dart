edae956c6634de2951b7a6547157fcc24444a4e4
Eniola Ajayi <ajayieniola16@gmail.com>
2020-06-14 08:55:29 +0100

    rebuilt screen and functions

--- lib/pages/payment_method_screen.dart
+++ lib/pages/payment_method_screen.dart
@@ -12,19 +12,26 @@ class PaymentMethodScreen extends StatefulWidget {
 }
 
 class _PaymentMethodScreenState extends State<PaymentMethodScreen> {
-  final AppBar _screenAppBar = AppBar(
-    leading: IconButton(
-        icon: Icon(Icons.keyboard_backspace),
-        iconSize: 30.0,
-        color: Colors.black,
-        onPressed: () {}),
-    title: Text(
-      'Payment Methods',
-      style: display2TextStyle,
-    ),
-    elevation: 0.0,
-    backgroundColor: Colors.white,
-  );
+//  keys
+  var _scaffoldKey = GlobalKey<ScaffoldState>();
+// snack bar
+  snackBar(value) {
+    return SnackBar(
+      content: Text(value),
+      duration: Duration(seconds: 3),
+      elevation: 23.0,
+    );
+  }
+
+// instance variables
+  bool isDeleteAppBar;
+  int paymentCardIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    isDeleteAppBar = false;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +39,7 @@ class _PaymentMethodScreenState extends State<PaymentMethodScreen> {
     final screenWidth = MediaQuery.of(context).size.width;
     UserProfileBloc userProfileBloc = Provider.of<UserProfileBloc>(context);
 
-    // PAYMENTS CARD BUILDER
+// Build card widgets
     Widget getCardContainer() {
       return Container(
         height: MediaQuery.of(context).size.height * 0.24,
@@ -48,41 +55,131 @@ class _PaymentMethodScreenState extends State<PaymentMethodScreen> {
                   itemBuilder: (BuildContext context, int index) {
                     PaymentCard paymentCard =
                         userProfileBloc.paymentCardList[index];
-                    return PaymentMethodCardWidget(
-                      cardBrandImgUrl: paymentCard.cardVendorImageUrl,
-                      cardNum: paymentCard.cardNum,
-                      cardType: paymentCard.getCardType(),
-                      isSelected: paymentCard.isSelected,
-                      onTap: () {
-                        setState(() {
-                          /*
-               To achieve effect found with radio buttons
-                */
-                          // on every card click
-                          // set state isSelected = false to deselect all selected cards
-                          userProfileBloc.paymentCardList
-                              .forEach((element) => element.isSelected = false);
-                          // then
-                          // set the selected card state isSelected = true
-                          paymentCard.isSelected = true;
-                        });
-                      },
-                    );
+                    if (paymentCard.isPayPal) {
+                      return InkWell(
+                        onTap: () {
+//                          get index num of that instance
+                          paymentCardIndex = userProfileBloc.paymentCardList
+                              .indexOf(paymentCard);
+                          print(paymentCardIndex);
+                          setState(() {
+                            userProfileBloc.paymentCardList
+                                .forEach((el) => el.isSelected = false);
+                            paymentCard.isSelected = true;
+                          });
+                        },
+                        onDoubleTap: () {
+                          setState(() {
+                            paymentCardIndex = userProfileBloc.paymentCardList
+                                .indexOf(paymentCard);
+                            userProfileBloc.paymentCardList
+                                .forEach((el) => el.isSelected = false);
+                            isDeleteAppBar = true;
+                          });
+                        },
+                        child: PaymentMethodCardWidget(
+                          isPayPal: paymentCard.isPayPal,
+                          cardBrandImgUrl: PaymentCardImgUrls.payPalImgUrl,
+                          cardDetail: paymentCard.email,
+                          cardType: paymentCard.getType(),
+                          isSelected: paymentCard.isSelected,
+                        ),
+                      );
+                    } else {
+                      return InkWell(
+                        onTap: () {
+                          setState(() {
+                            paymentCardIndex = userProfileBloc.paymentCardList
+                                .indexOf(paymentCard);
+                            userProfileBloc.paymentCardList
+                                .forEach((el) => el.isSelected = false);
+                            paymentCard.isSelected = true;
+                          });
+                        },
+                        onDoubleTap: () {
+                          print('recognises double tap');
+                          setState(() {
+                            paymentCardIndex = userProfileBloc.paymentCardList
+                                .indexOf(paymentCard);
+                            userProfileBloc.paymentCardList
+                                .forEach((el) => el.isSelected = false);
+                            isDeleteAppBar = true;
+                          });
+                        },
+                        child: PaymentMethodCardWidget(
+                          isPayPal: paymentCard.isPayPal,
+                          cardBrandImgUrl: paymentCard.cardVendorImageUrl,
+                          cardDetail: paymentCard.cardNum,
+                          cardType: paymentCard.getType(),
+                          isSelected: paymentCard.isSelected,
+                        ),
+                      );
+                    }
                   },
                 ),
               ),
       );
     }
-    final snackBar = SnackBar(content: Text('Yay! A SnackBar!'));
 
     return Scaffold(
-      appBar: _screenAppBar,
+      key: _scaffoldKey,
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+        leading: isDeleteAppBar
+            ? IconButton(
+                icon: Icon(Icons.close),
+                color: capitalVotesTheme().primaryColor,
+                onPressed: () {
+                  setState(() {
+                    isDeleteAppBar = false;
+                  });
+                },
+              )
+            : null,
+        title: Text(
+          isDeleteAppBar ? 'Delete' : 'Payment Methods',
+          style: TextStyle(
+            fontFamily: 'Poppins',
+            color: isDeleteAppBar
+                ? capitalVotesTheme().primaryColor
+                : Colors.white,
+            fontSize: 16.0,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        actions: isDeleteAppBar
+            ? <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(right: 20.0),
+                  child: IconButton(
+                    icon: Icon(Icons.delete),
+                    color: capitalVotesTheme().primaryColor,
+                    onPressed: () {
+                      setState(() {
+                        userProfileBloc
+                            .removeCardFromPaymentCardList(paymentCardIndex);
+                        isDeleteAppBar = false;
+//                        To check if delete functionality works
+                        print('was called to delete');
+                        print(paymentCardIndex);
+                        print(userProfileBloc.paymentCardList.length);
+                      });
+                    },
+                  ),
+                )
+              ]
+            : [],
+        elevation: 0.0,
+        backgroundColor:
+            isDeleteAppBar ? Colors.white : capitalVotesTheme().primaryColor,
+      ),
       body: ListView(
         physics: BouncingScrollPhysics(),
         children: <Widget>[
 //          PAYMENT CARDS CONTAINER
           getCardContainer(),
           Divider(thickness: 2.0, color: Colors.grey[200]),
+//          PAYMENT METHOD TITLE
           Padding(
             padding: const EdgeInsets.fromLTRB(20.0, 8.0, 0.0, 16.0),
             child: Text(
@@ -92,40 +189,53 @@ class _PaymentMethodScreenState extends State<PaymentMethodScreen> {
               ),
             ),
           ),
+//          PAYMENT METHOD OPTIONS
           Padding(
             padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
+//                ADD PAYMENT CARD ACCOUNT
                 PaymentMethodOptionWidget(
                   enabled: userProfileBloc.paymentCardList.length == 3
                       ? false
                       : true,
                   title: 'Add Credit or Debit Card',
-                  trailingImgUrl: PaymentMethodImages().mcDarkImgUrl,
+                  trailingImgUrl: PaymentCardImgUrls.mcDarkImgUrl,
                   onTap: () {
-                    userProfileBloc.paymentCardList.length == 3
-                        ? print('add no more cards')
-                        : pushGoTo(context, '/AddPaymentCardScreen');
+                    if (userProfileBloc.paymentCardList.length == 3) {
+                      _showInSnackBar('limit of three cards reached');
+                    } else {
+                      pushGoTo(context, '/AddPaymentCardScreen');
+                    }
                   },
                 ),
+//                ADD PAYPAL ACCOUNT BUTTON
                 PaymentMethodOptionWidget(
-                  enabled: true,
+                  enabled: userProfileBloc.paymentCardList.length == 3
+                      ? false
+                      : true,
                   title: 'Paypal',
-                  trailingImgUrl: PaymentMethodImages().payPalImgUrl,
+                  trailingImgUrl: PaymentCardImgUrls.payPalImgUrl,
                   onTap: () {
-                    pushGoTo(context, '/AddPayPalAccountScreen');
+                    if (userProfileBloc.paymentCardList.length == 3) {
+                      return _showInSnackBar('limit of three cards reached');
+                    } else {
+                      pushGoTo(context, '/AddPayPalAccountScreen');
+                    }
                   },
                 ),
+//                PROMO CODE BUTTON
                 PaymentMethodOptionWidget(
                   enabled: true,
                   title: 'Promo code',
-                  trailingImgUrl: PaymentMethodImages().promoCodeImgUrl,
+                  trailingImgUrl: PaymentCardImgUrls.promoCodeImgUrl,
                   onTap: () {
                     pushGoTo(context, '/PromoCodeScreen');
                   },
                 ),
+//                PRIVACY STATEMENT
                 Padding(
                   padding: EdgeInsets.fromLTRB(0.0, 16.0, 0.0, 20.0),
                   child: privacyStatementWidget,
@@ -139,14 +249,14 @@ class _PaymentMethodScreenState extends State<PaymentMethodScreen> {
               vertical: screenHeight * 0.07,
               horizontal: screenWidth * 0.08,
             ),
+//            PROCEED BUTTON
             child: RaisedButton(
-              
               child: Text('PROCEED'),
               color: capitalVotesTheme().primaryColor,
               textColor: Colors.white,
               disabledColor: Colors.white,
               disabledTextColor: Colors.black,
-              onPressed: () {},
+              onPressed: null,
               padding: EdgeInsets.symmetric(vertical: screenWidth * 0.05),
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(30.0),
@@ -156,6 +266,10 @@ class _PaymentMethodScreenState extends State<PaymentMethodScreen> {
         ],
       ),
     );
+  }
+
+  void _showInSnackBar(String value) {
+    _scaffoldKey.currentState.showSnackBar(snackBar(value));
   }
 }
 
@@ -182,61 +296,52 @@ Widget privacyStatementWidget = FittedBox(
 
 // Helper class to handle UI Design and state of payment cards
 class PaymentMethodCardWidget extends StatelessWidget {
-  final String cardNum;
+  final String cardDetail;
   final bool isSelected;
+  final bool isPayPal;
   final String cardBrandImgUrl, cardType;
-  final VoidCallback onTap;
 
   PaymentMethodCardWidget(
       {Key key,
       this.cardBrandImgUrl,
-      this.cardNum,
+      this.cardDetail,
       this.cardType,
-      this.onTap,
-      this.isSelected});
+      this.isSelected,
+      this.isPayPal});
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      radius: 300.0,
-      onTap: onTap,
-      child: Container(
-        width: MediaQuery.of(context).size.width * 0.285,
-        margin: EdgeInsets.symmetric(vertical: 6.0, horizontal: 8.0),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12.0),
-          border: isSelected
-              ? Border.all(
-                  color: capitalVotesTheme().primaryColor,
-                  width: 2.0,
-                )
-              : Border.all(color: Colors.white70),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey[300],
-              blurRadius: 2.0,
-              spreadRadius: 2.0,
-            ),
-          ],
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Image(
-              width: 60.0,
-              height: 25.0,
-              image: AssetImage(cardBrandImgUrl),
-            ),
-            Divider(
-              color: Colors.grey,
-              thickness: 1.0,
-            ),
-            Text('**${cardNum.substring(13)}'),
-            Text(cardType),
-          ],
-        ),
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.285,
+      margin: EdgeInsets.symmetric(vertical: 6.0, horizontal: 8.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12.0),
+        border: isSelected
+            ? Border.all(
+                color: capitalVotesTheme().primaryColor,
+                width: 2.0,
+              )
+            : Border.all(color: Colors.white70),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey[300],
+            blurRadius: 2.0,
+            spreadRadius: 2.0,
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Image(width: 60.0, height: 25.0, image: AssetImage(cardBrandImgUrl)),
+          Divider(color: Colors.grey, thickness: 1.0),
+          Text(isPayPal
+              ? cardDetail.substring(3)
+              : '***${cardDetail.substring(12)}'),
+          Text(isPayPal ? 'PayPal' : cardType),
+        ],
       ),
     );
   }
