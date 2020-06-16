2bc08a832b2736abcdd9993b3ea700d5e0187f2f
Eniola Ajayi <ajayieniola16@gmail.com>
2020-06-16 18:33:58 +0100

    edited vote cart screen

--- lib/pages/vote_cart_screen.dart
+++ lib/pages/vote_cart_screen.dart
@@ -37,74 +37,75 @@ class _VoteCartScreenState extends State<VoteCartScreen> {
   AssetImage _contestantImg = AssetImage('assets/images/users/user-1.jpg');
   String _slctedContestantName = 'Chloe Hermann';
   String _slctedContestantDesc = 'Lagos, Nigeria';
-  // WIDGETS
-  Widget slctedContestantInfo() {
-    double _bgContainerHeight = 90.0;
-    double _imgContainerWidth = 160.0;
-    double _imgContainerHeight = 240.0;
-    double _imgContainerRadius = 12.0;
-    BoxDecoration _imgContainerDecoration = BoxDecoration(
-      border: Border.all(color: Colors.white, width: 3.0),
-      borderRadius: BorderRadius.circular(_imgContainerRadius),
-    );
-    return Stack(
-      overflow: Overflow.visible,
-      children: <Widget>[
-//        Background container behind contestant image
-        Container(
-          width: double.infinity,
-          height: _bgContainerHeight,
-          decoration: BoxDecoration(
-            color: capitalVotesTheme().primaryColor,
-          ),
-        ),
-//        Contestant info  image && name && description
-        Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(top: 20.0),
-              ),
-              Container(
-                width: _imgContainerWidth,
-                height: _imgContainerHeight,
-                decoration: _imgContainerDecoration,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(_imgContainerRadius),
-                  child: Image(
-                    fit: BoxFit.cover,
-                    image: _contestantImg,
-                  ),
-                ),
-              ),
-              Padding(padding: EdgeInsets.only(top: 10.0)),
-              Text(
-                _slctedContestantName,
-                style: TextStyle(
-                  fontWeight: FontWeight.w700,
-                  fontSize: 14.5,
-                ),
-              ),
-              Text(_slctedContestantDesc, style: TextStyle(fontSize: 11.0)),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
 
+  static double _bgContainerHeight = 90.0;
+  static double _imgContainerWidth = 160.0;
+  static double _imgContainerHeight = 240.0;
+  static double _imgContainerRadius = 12.0;
+  BoxDecoration _imgContainerDecoration = BoxDecoration(
+    border: Border.all(color: Colors.white, width: 3.0),
+    borderRadius: BorderRadius.circular(_imgContainerRadius),
+  );
+
+  // WIDGETS
   @override
   Widget build(BuildContext context) {
     UserProfileBloc userProfileBloc = Provider.of<UserProfileBloc>(context);
-    ViewNomineeLocalBloc viewNomineeLocalBloc = Provider.of<ViewNomineeLocalBloc>(context);
+    ViewNomineeLocalBloc viewNomineeLocalBloc =
+        Provider.of<ViewNomineeLocalBloc>(context);
     return Scaffold(
       appBar: _screenAppBar,
       body: ListView(
         children: <Widget>[
           SizedBox(
             height: 350.0,
-            child: slctedContestantInfo(),
+            child: Stack(
+              overflow: Overflow.visible,
+              children: <Widget>[
+//        Background container behind contestant image
+                Container(
+                  width: double.infinity,
+                  height: _bgContainerHeight,
+                  decoration: BoxDecoration(
+                    color: capitalVotesTheme().primaryColor,
+                  ),
+                ),
+//        Contestant info  image && name && description
+                Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(top: 20.0),
+                      ),
+                      Container(
+                        width: _imgContainerWidth,
+                        height: _imgContainerHeight,
+                        decoration: _imgContainerDecoration,
+                        child: ClipRRect(
+                          borderRadius:
+                              BorderRadius.circular(_imgContainerRadius),
+                          child: Image(
+                            fit: BoxFit.cover,
+                            image: _contestantImg,
+                          ),
+                        ),
+                      ),
+                      Padding(padding: EdgeInsets.only(top: 10.0)),
+                      Text(
+                        _slctedContestantName,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 14.5,
+                        ),
+                      ),
+                      Text(_slctedContestantDesc,
+                          style: TextStyle(fontSize: 11.0)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
           VoteFunctionWidget(),
         ],
