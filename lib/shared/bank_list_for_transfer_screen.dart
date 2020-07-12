c1d71f9c0997a544fae69c8d2c5e796d6ea3bf70
Eniola Ajayi <ajayieniola16@gmail.com>
2020-07-12 05:38:31 +0100

    created bank list screen

---
+++ lib/shared/bank_list_for_transfer_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BankListScreen extends StatefulWidget {
+  @override
+  _BankListScreenState createState() => _BankListScreenState();
+}
+
+class _BankListScreenState extends State<BankListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
